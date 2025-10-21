efb43255fb4aceaa0e8a4617f6687715b60f2c3e
Rajat Jamgade <rajatjamgade00@gmail.com>
2025-10-22 00:42:24 +0530

    File Updated!!

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-<<<<<<< HEAD
 import Destination from "./Pages/destination";
 import Crew from "./Pages/crew";
-=======
-
-import Crew from "./Pages/crew";
-import Destination from "./Pages/destination";
->>>>>>> 13d94187807d2d4dec800b097899e7f21933d276
 import Technology from "./Pages/technology";
 
 const router = createBrowserRouter([
